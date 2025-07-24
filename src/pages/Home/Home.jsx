98c13ef8bc8dcfab2402f2d9a750bb0f1b67dec7
Ignacio <idavanzo@cegsoft.com>
2025-07-24 14:22:33 +0200

    hero fallback

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,21 @@
-import { useTranslation } from 'react-i18next';
-import { useMetaTags } from "../../services/useMetaTags";
-
-import Hero from "./components/Hero/Hero";
 import Services from "../Services/Services";
 import WhyChooseUs from "../WhyChooseUs/WhyChooseUs";
 import AboutUs from "../About/AboutUs";
 import Contact from "../Contact/Contact";
 import Faq from "../Faq/Faq";
-
+import React, { Suspense, Loader }  from 'react';;
 export default function Home() {
+  const Hero = React.lazy(() => import('./components/Hero/Hero'));
+
 
   return (
     <>
-      <Hero id="home" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Hero id="home" />
+      </Suspense>
       <Services id="services" />
       <WhyChooseUs />
-      <Faq id="faq"/>
+      <Faq id="faq" />
       <AboutUs id="aboutus" />
       <Contact id="contact" />
     </>
