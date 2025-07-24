e152f76a90abc41d32a7ba123ee023e1d6a286e1
Ignacio <idavanzo@cegsoft.com>
2025-07-24 14:26:54 +0200

    revert fallback

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,18 @@
+import Hero from "./components/Hero/Hero";
 import Services from "../Services/Services";
 import WhyChooseUs from "../WhyChooseUs/WhyChooseUs";
 import AboutUs from "../About/AboutUs";
 import Contact from "../Contact/Contact";
 import Faq from "../Faq/Faq";
-import React, { Suspense, Loader }  from 'react';;
-export default function Home() {
-  const Hero = React.lazy(() => import('./components/Hero/Hero'));
 
+export default function Home() {
 
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Hero id="home" />
-      </Suspense>
+      <Hero id="home" />
       <Services id="services" />
       <WhyChooseUs />
-      <Faq id="faq" />
+      <Faq id="faq"/>
       <AboutUs id="aboutus" />
       <Contact id="contact" />
     </>
